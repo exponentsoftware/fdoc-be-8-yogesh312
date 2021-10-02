472d2f1618e167aec0faad6e536520b527f92d63
yogesh <etpr.yogesh@gmail.com>
2021-10-02 18:19:58 +0530

    API routes for the app

---
+++ routes/user.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const { catchErrors } = require("../handlers/errorHandlers");
+const userController = require("../controllers/userController");
+
+router.post("/login", catchErrors(userController.login));
+router.get("/login", catchErrors(userController.logins));
+router.post("/register", catchErrors(userController.register));
+router.get("/register", catchErrors(userController.registers));
+
+router.get("/", catchErrors(userController.home));
+
+module.exports = router;
